284444d62c0d1faaba53bbe89f8c4117242ebc61
GhulamMustafa779 <mustafayousaf779@gmail.com>
2025-09-03 00:15:33 -0400

    running sum

--- level0/array/running-sum.py
+++ level0/array/running-sum.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+def runningSum(nums: list[int]) -> list[int]:
+    sum = 0
+    for i,n in enumerate(nums, start=0):
+        sum = sum + n
+        print(sum)
+        nums[i] = sum
+        
+    return nums
+
+
+
+print(runningSum([1,2,3,4]))
