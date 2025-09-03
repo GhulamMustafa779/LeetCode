3d41eaadbbc13550cfd7380199668f519e12b08d
GhulamMustafa779 <mustafayousaf779@gmail.com>
2025-09-03 06:06:17 -0400

    good pairs

---
+++ level0/array/good-pairs.py
@@ -0,0 +1,11 @@
+def numIdenticalPairsArray(nums: list[int]) -> int:
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j]:
+                count+=1
+    
+    return count
+
+
+print(numIdenticalPairsArray([1,1,1,1]))
