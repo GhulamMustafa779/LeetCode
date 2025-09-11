9699158ac8daa97e741e1b599718045bfbd989e0
GhulamMustafa779 <mustafayousaf779@gmail.com>
2025-09-11 09:38:44 -0400

    reverse string

---
+++ level0/string/reverse-string.py
@@ -0,0 +1,13 @@
+def reverseString(s: list[str]) -> None:
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+
+s = ["h","e","l","l","o"]
+reverseString(s)
+print(s)
